829bf0f9ebd3e7fd57afee50315a46b839b656da
Francisco <poli.ferreras@gmail.com>
2019-02-26 19:10:03 +0100

    Arreglando errores

--- Acme-Madruga/src/main/java/controllers/brotherhood/ProcessionBrotherhoodController.java
+++ Acme-Madruga/src/main/java/controllers/brotherhood/ProcessionBrotherhoodController.java
@@ -1,5 +1,6 @@
 package controllers.brotherhood;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 import javax.validation.Valid;
@@ -45,6 +46,27 @@ public class ProcessionBrotherhoodController extends AbstractController {
 		result.addObject("processions", processions);
 		result.addObject("requestURI", "procession/brotherhood/list.do");
 
+		return result;
+	}
+
+	// List
+	@RequestMapping(value = "/list", method = RequestMethod.GET)
+	public ModelAndView list(@RequestParam final int brotherhoodId) {
+		ModelAndView result;
+		Collection<Procession> processions;
+		try {
+			processions = new ArrayList<Procession>();
+			processions = this.processionService
+					.findAllProcessionsOfOneBrotherhood(brotherhoodId);
+			result = new ModelAndView("procession/list");
+			result.addObject("processions", processions);
+			result.addObject("requestURI", "procession/brotherhood/list.do");
+		} catch (final Throwable oops) {
+			oops.printStackTrace();
+			result = new ModelAndView("procession/list");
+			result.addObject("message", "procession.retrieve.error");
+			result.addObject("processions", new ArrayList<FloatB>());
+		}
 		return result;
 	}
 
@@ -130,52 +152,4 @@ public class ProcessionBrotherhoodController extends AbstractController {
 		return result;
 	}
 
-}
-
-
-package controllers.brotherhood;
-
-import java.util.ArrayList;
-import java.util.Collection;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
-
-import services.ProcessionService;
-import controllers.AbstractController;
-import domain.FloatB;
-import domain.Procession;
-
-@Controller
-@RequestMapping("/procession/brotherhood")
-public class ProcessionBrotherhoodController extends AbstractController {
-
-	// Servicios
-	@Autowired
-	private ProcessionService	processionService;
-
-
-	// List
-	@RequestMapping(value = "/list", method = RequestMethod.GET)
-	public ModelAndView list(@RequestParam final int brotherhoodId) {
-		ModelAndView result;
-		Collection<Procession> processions;
-		try {
-			processions = new ArrayList<Procession>();
-			processions = this.processionService.findAllProcessionsOfOneBrotherhood(brotherhoodId);
-			result = new ModelAndView("procession/list");
-			result.addObject("processions", processions);
-			result.addObject("requestURI", "procession/brotherhood/list.do");
-		} catch (final Throwable oops) {
-			oops.printStackTrace();
-			result = new ModelAndView("procession/list");
-			result.addObject("message", "procession.retrieve.error");
-			result.addObject("processions", new ArrayList<FloatB>());
-		}
-		return result;
-	}
 }
