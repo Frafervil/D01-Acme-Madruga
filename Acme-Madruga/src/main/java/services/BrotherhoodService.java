2cedc369deb52c4183d6b9d609cb20ac12262bc6
Unknown <Ivan_LH97@hotmail.com>
2019-03-28 01:52:57 +0100

    Telefono arreglado

--- Acme-Madruga/src/main/java/services/BrotherhoodService.java
+++ Acme-Madruga/src/main/java/services/BrotherhoodService.java
@@ -3,9 +3,9 @@ package services;
 
 import java.util.Collection;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.HashMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
@@ -186,7 +186,6 @@ public class BrotherhoodService {
 		result.setEmail(brotherhoodForm.getEmail());
 		result.setMiddleName(brotherhoodForm.getMiddleName());
 		result.setName(brotherhoodForm.getName());
-		result.setPhone(brotherhoodForm.getPhone());
 		result.setPhoto(brotherhoodForm.getPhoto());
 		result.setPictures(brotherhoodForm.getPictures());
 		result.setSurname(brotherhoodForm.getSurname());
@@ -199,6 +198,7 @@ public class BrotherhoodService {
 			if (matcher.matches())
 				brotherhoodForm.setPhone(this.customisationRepository.findAll().iterator().next().getCountryCode() + brotherhoodForm.getPhone());
 		}
+		result.setPhone(brotherhoodForm.getPhone());
 
 		if (!brotherhoodForm.getPassword().equals(brotherhoodForm.getPasswordChecker()))
 			binding.rejectValue("passwordChecker", "brotherhood.validation.passwordsNotMatch", "Passwords doesnt match");
@@ -226,11 +226,18 @@ public class BrotherhoodService {
 		result.setMessageBoxes(brotherhood.getMessageBoxes());
 		result.setMiddleName(brotherhood.getMiddleName());
 		result.setName(brotherhood.getName());
-		result.setPhone(brotherhood.getPhone());
 		result.setPhoto(brotherhood.getPhoto());
 		result.setSurname(brotherhood.getSurname());
 		result.setPictures(brotherhood.getPictures());
 		result.setTitle(brotherhood.getTitle());
+
+		if (!StringUtils.isEmpty(brotherhood.getPhone())) {
+			final Pattern pattern = Pattern.compile("^\\d{4,}$", Pattern.CASE_INSENSITIVE);
+			final Matcher matcher = pattern.matcher(brotherhood.getPhone());
+			if (matcher.matches())
+				brotherhood.setPhone(this.customisationRepository.findAll().iterator().next().getCountryCode() + brotherhood.getPhone());
+		}
+		result.setPhone(brotherhood.getPhone());
 		this.validator.validate(result, binding);
 		this.brotherhoodRepository.flush();
 		return result;
